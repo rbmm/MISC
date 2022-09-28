d4cabb27f29a01715c6635d8342b35ad3f6ab812
rbmm <8209FBFB017DB41D@yopmail.com>
2022-09-28 16:13:28 +0300

    Update my#1.cpp

--- FreeRDP/V#1/my#1.cpp
+++ FreeRDP/V#1/my#1.cpp
@@ -3,7 +3,11 @@
 #include <stdio.h>
 #include <malloc.h>
 
-#pragma warning(disable : 4706 4094) // assignment within conditional expression
+#if 0
+
+#include <freerdp/settings.h>
+
+#else
 
 #define FreeRDP_ServerHostname (20)
 #define FreeRDP_Username (21)
@@ -14,23 +18,51 @@
 
 typedef struct rdp_settings rdpSettings;
 
-EXTERN_C_START
-
 const char* freerdp_settings_get_string(const rdpSettings* settings, size_t id);
 BOOL freerdp_settings_set_string(rdpSettings* settings, size_t id, const char* param);
 
-EXTERN_C_END
+#endif
 
-PCWSTR ValidateString(const BYTE* pb, ULONG cb)
+#pragma warning( disable : 4706 ) // assignment within conditional expression
+
+BOOL freerdp_settings_set_string_W(rdpSettings* settings, size_t id, PCWSTR param)
 {
-	return pb &&							// present ?
-		cb &&								// size not 0 ?
-		!(cb & (sizeof(WCHAR) - 1)) &&		// cb == n * sizeof(WCHAR) ?
-		!*(WCHAR*)(pb + cb - sizeof(WCHAR))	// 0 terminated ?
-		? (PCWSTR)pb : 0;					
+	PSTR lpMultiByteStr = 0;
+	int cbMultiByte = 0;
+
+	while (cbMultiByte = WideCharToMultiByte(CP_UTF8, 0, param, -1, lpMultiByteStr, cbMultiByte, 0, 0))
+	{
+		if (lpMultiByteStr)
+		{
+			return freerdp_settings_set_string(settings, id, lpMultiByteStr);
+		}
+
+		lpMultiByteStr = (PSTR)alloca(cbMultiByte);
+	}
+
+	return FALSE;
 }
 
-void AddDefaultSettings_I(rdpSettings* settings, size_t idHostname, size_t idUsername, size_t idPassword)
+static PWSTR ValidateString(const BYTE* pb, ULONG cb)
+{
+	if (pb)
+	{
+		if (cb)
+		{
+			if (!(cb & (sizeof(WCHAR) - 1)))
+			{
+				if (!*(WCHAR*)(pb + cb - sizeof(WCHAR)))
+				{
+					return (PWSTR)pb;
+				}
+			}
+		}
+	}
+
+	return 0;					
+}
+
+static void AddDefaultSettings_I(rdpSettings* settings, size_t idHostname, size_t idUsername, size_t idPassword)
 {
 	PCSTR ServerHostname = freerdp_settings_get_string(settings, idHostname);
 
@@ -47,48 +79,33 @@ void AddDefaultSettings_I(rdpSettings* settings, size_t idHostname, size_t idUse
 		return ;
 	}
 
-	PCREDENTIALA Credential;
-	PCWSTR lpWideCharStr;
+	PWSTR ServerHostnameW = 0;
+	PWSTR TargetName = 0;
+	ULONG len = 0;
 
-	PSTR Password;
-	PSTR UserName;
-
-	PSTR TargetName = 0;
-	int len = 0;
-
-
-	while (0 < (len = _vsnprintf(TargetName, len, "TERMSRV/%s", (va_list)&ServerHostname)))
+	while (len = MultiByteToWideChar(CP_UTF8, 0, ServerHostname, -1, ServerHostnameW, len))
 	{
 		if (TargetName)
 		{
-			if (CredReadA(TargetName, CRED_TYPE_GENERIC, 0, &Credential))
+			PCREDENTIALW Credential;
+
+			if (CredReadW(TargetName, CRED_TYPE_GENERIC, 0, &Credential))
 			{
 				if (!bExistPassword)
 				{
 					ULONG cch = Credential->CredentialBlobSize;
 
-					if (lpWideCharStr = ValidateString(Credential->CredentialBlob, cch))
+					if (TargetName = ValidateString(Credential->CredentialBlob, cch))
 					{
-						Password = 0, len = 0, cch /= sizeof(WCHAR);
-
-						while (len = WideCharToMultiByte(CP_UTF8, 0, lpWideCharStr, cch, Password, len, 0, 0))
-						{
-							if (Password)
-							{
-								freerdp_settings_set_string(settings, idPassword, Password);
-								break;
-							}
-
-							Password = (PSTR)alloca(len);
-						}
+						freerdp_settings_set_string_W(settings, idPassword, TargetName);
 					}
 				}
 
 				if (!bExistUserName)
 				{
-					if (UserName = Credential->UserName)
+					if (TargetName = Credential->UserName)
 					{
-						freerdp_settings_set_string(settings, idUsername, UserName);
+						freerdp_settings_set_string_W(settings, idUsername, TargetName);
 					}
 				}
 
@@ -98,12 +115,17 @@ void AddDefaultSettings_I(rdpSettings* settings, size_t idHostname, size_t idUse
 			break;
 		}
 
-		TargetName = (PSTR)alloca(++len);
+		static const WCHAR TERMSRV[] = L"TERMSRV/";
+
+		TargetName = alloca(sizeof(TERMSRV) - sizeof(WCHAR) + len * sizeof(WCHAR));
+
+		memcpy(TargetName, TERMSRV, sizeof(TERMSRV) - sizeof(WCHAR));
+
+		ServerHostnameW = TargetName + _countof(TERMSRV) - 1;
 	}
 }
 
-
-EXTERN_C void WINAPI AddDefaultSettings(_Inout_ rdpSettings* settings)
+void WINAPI AddDefaultSettings(_Inout_ rdpSettings* settings)
 {
 	AddDefaultSettings_I(settings, FreeRDP_ServerHostname, FreeRDP_Username, FreeRDP_Password);
 	AddDefaultSettings_I(settings, FreeRDP_GatewayHostname, FreeRDP_GatewayUsername, FreeRDP_GatewayPassword);
